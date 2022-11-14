b2963a02dfcad4c62ff395c3067b28c9a2185acd
Haritha Diraneyya <haritha.diraneyya@gmail.com>
2022-11-14 18:59:58 -0500

    added Slack logo to joi chat - experimental

--- .eleventy.js
+++ .eleventy.js
@@ -77,7 +77,8 @@ module.exports = function(eleventyConfig) {
   // Copy Node Modules dependencies
   eleventyConfig.addPassthroughCopy({
     "node_modules/aos/dist/aos.css": "_assets/css/aos.css",
-    "node_modules/aos/dist/aos.js": "_assets/js/aos.js"
+    "node_modules/aos/dist/aos.js": "_assets/js/aos.js",
+    "node_modules/aos/dist/fontawesome.js": "https://kit.fontawesome.com/f8f49d4cdb.js"
   });
 
   eleventyConfig.setBrowserSyncConfig({
